2a5f9f663f181c291c54c283f38afbca50b43470
juanK1011 <jj.rincons1@uniandes.edu.co>
2026-08-31 12:18:25 -0500

    cambios queue

--- DataStructures/queue.py
+++ DataStructures/queue.py
@@ -1,1 +1,19 @@
 
+
+def new_queue():
+    new_queue ={"size": 0,
+             "elements" : []
+             }
+    return new_queue
+
+def enqueue (my_queue, element):
+    my_queue["elements"].append(element)
+    my_queue["size"] += 1
+    return my_queue
+
+
+
+
+    
+
+
