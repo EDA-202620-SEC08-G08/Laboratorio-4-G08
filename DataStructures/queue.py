9ea98f010058dfec07f0bc2130ecae8eb31a6a54
juanK1011 <jj.rincons1@uniandes.edu.co>
2026-09-03 01:03:04 -0500

    cambios queue

--- DataStructures/queue.py
+++ DataStructures/queue.py
@@ -11,7 +11,17 @@ def enqueue (my_queue, element):
     my_queue["size"] += 1
     return my_queue
 
+def dequeue (my_queue, element):
+    remove = my_queue["elements"].pop(0)
+    my_queue["size"] -= 1
+    return remove
 
+def is_empty (my_queue):
+    vacia = True
+    if my_queue["size"] == 0:
+        vacia = False
+    return vacia
+    
 
 
     
